ec675704626976101b4d1eb8eb1e51d0bf11d24b
Blair Learn <blairlearn@users.noreply.github.com>
2010-09-20 18:38:38 +0000

    CMSManager.csproj -- Add dependencies for constants.cs and WorkflowController.cs.
    constants.cs -- string constants for named objects. (Avoids errors with spelling and casing.)
    
    WorkflowController.cs -- Controls a content item's progress through the CancerGov_PDQ_Workflow.

---
+++ Core/CMSManager/CMS/WorkflowController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GKManagers.CMSManager.CMS
+{
+    /// <summary>
+    /// Controls a content item's progress through the CancerGov_PDQ_Workflow
+    /// </summary>
+    internal class WorkflowController
+    {
+    }
+}
---
+++ Core/CMSManager/CMS/constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GKManagers.CMSManager.CMS
+{
+    // Collection of object names.  (Avoids spelling and casing problems.)
+
+    internal class WorkFlowStateNames
+    {
+        public const string Staging = "Staging";
+        public const string Preview = "Preview";
+        public const string Live = "Live";
+        public const string UpdateStaging = "UpdateStaging";
+        public const string UpdateStaging = "UpdatePreview";
+    }
+}
