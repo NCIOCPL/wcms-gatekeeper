021338b6e18082ba1b34017fa84c848f84eb6195
Blair Learn <blairlearn@users.noreply.github.com>
2010-09-21 18:45:59 +0000

    Initial stubs for IDMapManager.cs and IDMapQuery.cs.

---
+++ Core/CMSManager/IDMapManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GKManagers.CMSManager
+{
+    class IDMapManager
+    {
+        private IDMapQuery m_query = new IDMapQuery();
+
+        public bool InsertCdrIDMapping(int cdrid, long cmsid, string prettyURL)
+        {
+            return m_query.InsertCdrIDMapping(cdrid, cmsid, prettyURL);
+        }
+    }
+}
---
+++ Core/CMSManager/IDMapQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GKManagers.CMSManager
+{
+    internal class IDMapQuery
+    {
+        public bool InsertCdrIDMapping(int cdrid, long cmsid, string prettyURL)
+        {
+            bool success = true;
+
+            return success;
+        }
+    }
+}
