f6b4b6d074892a61c8670cc424ccd8cf058ac0cd
mungarark <webcommauto@users.noreply.github.com>
2010-09-21 20:09:29 +0000

    Updated class constructor

--- Core/CMSManager/CMS/ContentMetaItem.cs
+++ Core/CMSManager/CMS/ContentMetaItem.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Collections.ObjectModel;
 namespace GKManagers.CMSManager.CMS
 {
     public class ContentMetaItem
     {
-
+        public ContentMetaItem(long id,Dictionary<string,string> fields)
+        {
+            ID = id;
+            Fields = fields;
+        }
         public long ID {get;private set;}
-
-
+        public Dictionary<string, string> Fields { get; private set; }
+        
     }
 }
