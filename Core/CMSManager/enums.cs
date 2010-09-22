12d8d12e8f56e1a1df027142e5d600909aaa59d8
Blair Learn <blairlearn@users.noreply.github.com>
2010-09-22 23:23:29 +0000

    constants.cs -- No longer needed.
    enums.cs -- Enumerated values for WorkFlow state and transition names.
    
    CMSManager.csproj -- add/remove files as needed.

---
+++ Core/CMSManager/enums.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GKManagers.CMSManager
+{
+    public enum WorkflowState
+    {
+        Invalid = 0,
+        Staging = 1,
+        Preview = 2,
+        Live = 3,
+        UpdateStaging = 4,
+        UpdatePreview = 5
+    }
+
+    public enum WorkflowTransition
+    {
+        Invalid = 0,
+
+        // From Staging
+        PromoteToPreviewNew = 1,
+
+        // From Preview
+        PromoteToLiveNew = 2,
+        RevertToStagingNew = 3,
+
+        // From Live
+        Update = 4,
+
+        // From UpdateStaging
+        PromoteToPreviewUpdate = 5,
+
+        // From UpdatePreview
+        PromoteToLiveUpdate = 6,
+        RevertToStagingUpdate = 7
+    }
+}
