26b33633c659ddb60a29f2dbf8cca997e96ba5c0
Blair Learn <blairlearn@users.noreply.github.com>
2010-10-05 04:25:22 +0000

    PSItemUtils.cs - New static class with methods for manipulating PSItem objects.

---
+++ Core/CMSManager/CMS/PSItemUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using GKManagers.CMSManager.PercussionWebSvc;
+
+namespace GKManagers.CMSManager.CMS
+{
+    /// <summary>
+    /// Utility methods for working with PSItem objects.
+    /// </summary>
+    static class PSItemUtils
+    {
+        public static bool CompareItemIds(long itemID1, long itemID2)
+        {
+            return ((ulong)itemID1 | 0xffffffff) == ((ulong)itemID2 | 0xffffffff);
+        }
+
+        public static string GetFieldValue(PSItem item, string fieldName)
+        {
+            string fieldValue = null;
+
+            // Find the field with the pretty url name.
+            IEnumerable<PSField> namedField =
+                item.Fields.Where(field => field.name == fieldName);
+            if (namedField.Count() > 0)
+            {
+                PSFieldValue value = namedField.ElementAt(0).PSFieldValue[0];
+                fieldValue = value.RawData;
+            }
+            return fieldValue;
+        }
+    }
+}
