7715c0bf4e3a924d8688bbc3e2dda525e5a247f2
Blair Learn <blairlearn@users.noreply.github.com>
2010-12-08 20:29:09 +0000

    Move DocumentTypeFlag to a separate file.

--- Core/DocumentManager/DocumentCacheSupervisor.cs
+++ Core/DocumentManager/DocumentCacheSupervisor.cs
@@ -6,29 +6,6 @@ using GKManagers.BusinessObjects;
 
 namespace GKManagers
 {
-    #region DocumentTypeFlag
-
-    [Flags]
-    internal enum DocumentTypeFlag
-    {
-        Empty = 0x00,
-        GlossaryTerm = 0x01,
-        CTGovProtocol = 0x02,
-        Media = 0x04,
-        Term = 0x08,
-        DrugInformationSummary = 0x10,
-        InScopeProtocol = 0x20,
-        Summary = 0x40,
-        Organization = 0x80,
-        MiscellaneousDocument = 0x100,
-        OutOfScopeProtocol = 0x200,
-        PoliticalSubUnit = 0x400,
-        GeneticsProfessional = 0x800,
-        Protocol = 0x1000
-    }
-
-    #endregion
-
     class DocumentTypeTracker
     {
         private DocumentTypeFlag _documentTypes = DocumentTypeFlag.Empty;
---
+++ Core/DocumentManager/DocumentTypeFlag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GKManagers
+{
+    [Flags]
+    internal enum DocumentTypeFlag
+    {
+        Empty = 0x00,
+        GlossaryTerm = 0x01,
+        CTGovProtocol = 0x02,
+        Media = 0x04,
+        Term = 0x08,
+        DrugInformationSummary = 0x10,
+        InScopeProtocol = 0x20,
+        Summary = 0x40,
+        Organization = 0x80,
+        MiscellaneousDocument = 0x100,
+        OutOfScopeProtocol = 0x200,
+        PoliticalSubUnit = 0x400,
+        GeneticsProfessional = 0x800,
+        Protocol = 0x1000
+    }
+}
