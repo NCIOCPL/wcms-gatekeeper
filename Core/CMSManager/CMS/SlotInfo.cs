29aa00d3aee7b846a92bc11b50aad6a1fb81d8d7
Blair Learn <blairlearn@users.noreply.github.com>
2011-01-16 18:54:08 +0000

    SlotInfo.cs   Rename ID property to make distinct from the properties of the Percussion Guid type.

--- Core/CMSManager/CMS/SlotInfo.cs
+++ Core/CMSManager/CMS/SlotInfo.cs
@@ -4,7 +4,7 @@ namespace NCI.WCM.CMSManager.CMS
 {
     public class SlotInfo
     {
-        public PercussionGuid ID { get; private set; }
+        public PercussionGuid CmsGuid { get; private set; }
         public string Name { get; private set; }
 
         public List<ContentTypeToTemplateInfo> AllowedContentTemplatePairs = new List<ContentTypeToTemplateInfo>();
@@ -12,7 +12,7 @@ namespace NCI.WCM.CMSManager.CMS
         public SlotInfo(string name, PercussionGuid id)
         {
             Name = name;
-            ID = id;
+            CmsGuid = id;
         }
     }
 }
