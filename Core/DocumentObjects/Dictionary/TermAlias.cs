98e1d93dceb41d24c10723a0922ccd206ad89f75
Blair Learn <blairlearn@users.noreply.github.com>
2015-06-22 17:03:56 +0000

    Initial class definitions for dictionary entries.

---
+++ Core/DocumentObjects/Dictionary/TermAlias.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GateKeeper.DocumentObjects.Dictionary
+{
+    // Class representing a single alias for a given dictionary term
+    public class TermAlias
+    {
+        public String AlternateName { get; set; }
+        public String NameType { get; set; }
+        public String Language { get; set; }
+    }
+}
