659d5b70206302b0f5651e450b3d449fd0ac9790
Blair Learn <blairlearn@users.noreply.github.com>
2015-06-22 15:53:33 +0000

    Remedy problems caused by missing VisualSVN installation.

---
+++ Core/DocumentObjects/Dictionary/Dictionary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GateKeeper.DocumentObjects.Dictionary
+{
+    public class Dictionary
+    {
+    }
+}
