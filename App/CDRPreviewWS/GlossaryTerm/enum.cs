fae2a58075848709cea03003dc97ea8b9eca2de6
shahaj <webcommauto@users.noreply.github.com>
2015-09-25 15:21:48 +0000

    OCEProject 3617 - Copying the deserialization business objects from CDE to GK for Glossary Term Publish Preview

---
+++ App/CDRPreviewWS/GlossaryTerm/enum.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NCI.Web.Dictionary
+{
+    /// <summary>
+    /// Enumeration of the known dictionary types.
+    /// </summary>
+    public enum DictionaryType
+    {
+        // We don't know what dictionary this is.  Error condition.
+        Unknown = 0,
+
+        // Dictionary of Cancer Terms
+        term = 1,
+
+        // Drug Dictionary
+        drug = 2,
+
+        // Dictionary of Genetics Terms
+        genetic = 3
+    }
+
+    /// <summary>
+    /// Allowed search types
+    /// </summary>
+    public enum SearchType
+    {
+        Unknown = 0,
+
+        /// <summary>
+        /// Search for terms beginning with
+        /// </summary>
+        Begins = 1,
+
+        /// <summary>
+        /// Search for terms containing
+        /// </summary>
+        Contains = 2,
+
+        /// <summary>
+        /// Search for terms beginning with, followed by terms containing
+        /// </summary>
+        Magic = 3
+    }
+
+    public enum AudienceType
+    {
+        Unknown = 0,
+        Patient = 1,
+        HealthProfessional = 2
+    }
+}
