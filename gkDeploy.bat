4426ead78a773befaf251cb1c8e7ae6a1ef4928f
Blair Learn <blairlearn@users.noreply.github.com>
2017-03-15 19:30:27 +0000

    Backup GateKeeper, not cde

--- gkDeploy.bat
+++ gkDeploy.bat
@@ -1,6 +1,6 @@
 @echo off
 rem Do a backup first.
-powershell -ExecutionPolicy RemoteSigned ./cdeBackup.ps1
+powershell -ExecutionPolicy RemoteSigned ./gkBackup.ps1
 setLocal
 rem Build up the source location from the current execution path and
 rem an assumed DATA subdirectory.
