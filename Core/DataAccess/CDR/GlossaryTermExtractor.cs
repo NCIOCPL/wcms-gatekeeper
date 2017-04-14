b2bcd9f0f6d79f9a73131f2c499d5d892d920c78
Blair Learn <blairlearn@users.noreply.github.com>
2017-04-14 21:17:39 +0000

    Modify GlossaryTermExtractor to expect the language and audience values to be supplied by the CDR rather than looking them up in the database.
    [WCMSGK-40]

--- Core/DataAccess/CDR/GlossaryTermExtractor.cs
+++ Core/DataAccess/CDR/GlossaryTermExtractor.cs
@@ -61,9 +61,6 @@ namespace GateKeeper.DataAccess.CDR
                 GeneralDictionaryEntry[] dictionary = GetDictionary(extractData);
                 glossaryTermDocument.Dictionary.AddRange(dictionary);
 
-                // Hack to rewrite related Glosssary Term elements.
-                RewriteRelatedGlossaryTerms(xmlDoc, document);
-
                 // Copy the the XML document into the Glossary Term business object because that's
                 // where all the rest of the code expects to find it.
                 DocumentHelper.CopyXml(xmlDoc, glossaryTermDocument);
@@ -72,71 +69,6 @@ namespace GateKeeper.DataAccess.CDR
             {
                 throw new ExtractionException("Extraction Error: Failed to extract glossary term document", e);
             }
-        }
-
-        /// <summary>
-        /// Hack to rewrite the XML document's references to related Glossary Terms so they can be
-        /// rendered more correctly in the JSON. (NOTE: The XML is rewritten based on the languages
-        /// and audiences of any related terms. It is possible that related term references will be
-        /// written for language and audiences that the current document knows nothing about.)
-        /// </summary>
-        /// <param name="xmlDoc"></param>
-        /// <param name="document"></param>
-        private void RewriteRelatedGlossaryTerms(XmlDocument xmlDoc, Document gtDocument)
-        {
-            XPathNavigator nav = xmlDoc.CreateNavigator();
-            XPathNodeIterator relatedTerms = nav.Select("//RelatedInformation/RelatedGlossaryTermRef");
-
-            // Template for writing new RelatedGlossaryTermRef elements.
-            string TermElementFmt = "<RelatedGlossaryTermRef href=\"{0}\" UseWith=\"{1}\" audience=\"{2}\">{3}</RelatedGlossaryTermRef>";
-
-            List<XPathNavigator> oldEntries = new List<XPathNavigator>();
-
-            foreach (XPathNavigator term in relatedTerms)
-            {
-                // Keep a list of pre-rewrite entries for eventual clean-up.
-                oldEntries.Add(term);
-
-                // Get related Term ID.
-                string idAsString = DocumentHelper.GetAttribute(term, "href");
-                int relatedID;
-                if (!Int32.TryParse(CDRHelper.ExtractCDRID(idAsString), out relatedID))
-                    throw new Exception("Failed to parse" + term.Name + " document id from value: " + idAsString);
-
-                // Fetch the referenced glossary term so we can determine audiences, languages, etc.
-                GlossaryTermSimple relatedTerm = getRelatedGlossaryTerm(relatedID, gtDocument.WarningWriter);
-
-                if (relatedTerm == null)
-                {
-                    gtDocument.WarningWriter(String.Format("RewriteRelatedGlossaryTerms: Unable to fetch related term '{0}'.", relatedID));
-                    continue;
-                }
-
-
-                // Insert new nodes for specific languages and audiences *before* the current node.
-                // (Inserting after would cause them to be picked up as next in the list, repeating
-                // the loop endlessly.)
-                if (relatedTerm.HasPatientDefinition)
-                {
-                    // We assume the related term has an English version.
-                    term.InsertBefore(String.Format(TermElementFmt, idAsString, "en", AudienceType.Patient, relatedTerm.Name));
-                    if(relatedTerm.HasSpanishTerm)
-                        term.InsertBefore(String.Format(TermElementFmt, idAsString, "es", AudienceType.Patient, relatedTerm.SpanishName));
-                }
-
-                if (relatedTerm.HasHealthProfessionalDefinition)
-                {
-                    // We assume the related term has an English version.
-                    // Currently, no dicitonary uses Spanish with a Health Professional definition.
-                    term.InsertBefore(String.Format(TermElementFmt, idAsString, "en", AudienceType.HealthProfessional, relatedTerm.Name));
-                }
-
-                // Remove the orignal element.
-                //term.DeleteSelf();
-            }
-
-            // Remove the original entries from the XML.  (Deleting them earlier would invalidate the iterator.)
-            oldEntries.ForEach(entry => { entry.DeleteSelf(); });
         }
 
         /// <summary>
@@ -162,23 +94,6 @@ namespace GateKeeper.DataAccess.CDR
             }
 
             return dictionary.ToArray();
-        }
-
-
-        private GlossaryTermSimple getRelatedGlossaryTerm(int documentId, HistoryEntryWriter logWriter)
-        {
-            GlossaryTermSimple term = null;
-            try
-            {
-                GlossaryTermQuery query = new GlossaryTermQuery();
-                term = query.GetGlossaryTerm(documentId);
-            }
-            catch (Exception ex)
-            {
-                // Swallow the exception.  Not finding the related term shouldn't be fatal.
-                logWriter("HistoryEntryWriter.getRelatedGlossaryTerm(). Error retrieveing documentId = " + documentId + ". " + ex.ToString());
-            }
-            return term;
         }
     
     }
