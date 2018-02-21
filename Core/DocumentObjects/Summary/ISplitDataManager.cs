c1e7dadf50a536531d0f60a5ddb1b7f3129fc5ac
Blair <blairlearn@users.noreply.github.com>
2018-02-21 11:10:15 -0500

    Clean up using statements.

--- Core/DocumentObjects/Summary/ISplitDataManager.cs
+++ Core/DocumentObjects/Summary/ISplitDataManager.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GateKeeper.DocumentObjects.Summary
+﻿namespace GateKeeper.DocumentObjects.Summary
 {
     /// <summary>
     /// Interface definition for an object which contains data about which
